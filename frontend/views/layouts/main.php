ce28ae8730715dacc791a7b5d17e3e7a5408a694
t1mon <gorin163@gmail.com>
2017-12-22 18:49:52 +0400

    google analytics

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -21,16 +21,25 @@ AppAsset::register($this);
 <!DOCTYPE html>
 <html lang="ru-RU">
 <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111095563-1"></script>
+    <!--Google analytics-->
     <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        gtag('config', 'UA-111095563-1');
-    </script>
+        ga('create', 'UA-111095563-1', 'auto'); //ТУТ МЕНЯТЬ НОМЕР НА СВОЙ
+        ga('require', 'displayfeatures');
+        ga('send', 'pageview');
 
+        /* Accurate bounce rate by time */
+        if (!document.referrer ||
+            document.referrer.split('/')[2].indexOf(location.hostname) != 0)
+            setTimeout(function(){
+                ga('send', 'event', 'Новый посетитель', location.pathname);
+            }, 15000);</script>
+
+    <!--Google analytics-->
 <meta charset="<?= Yii::$app->charset ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="yandex-verification" content="5a09ace581bf0355" />
