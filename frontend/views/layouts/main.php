1e876bb7588f06099617a04493b3bd938a386827
t1mon <gorin163@gmail.com>
2018-03-07 00:44:28 +0400

    Add Yandex Widget IP

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -87,8 +87,15 @@ AppAsset::register($this);
     </script>
     <noscript><div><img src="https://mc.yandex.ru/watch/46982373" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <!-- /Yandex.Metrika counter -->
+    <script type="text/javascript">
+        document.write(unescape("%3Cscript src='" + "//yandex.mightycall.ru/CallbackWidget/js/MightyCallCBW.js' type='text/javascript'%3E%3C/script%3E"));
+        document.write(unescape("%3Cscript src='" + "//mightycallstorage.blob.core.windows.net/cbw/8ba1dc5d-7385-4c5c-b937-bacd5fe71948__widget.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+        InitCallbackWidget();
+    </script>
     <!-- pozvonim-->
-    <script crossorigin="anonymous" async type="text/javascript" src="//api.pozvonim.com/widget/callback/v3/6ec34d3f2c2b1ff57be222c4315105fb/connect" id="check-code-pozvonim" charset="UTF-8"></script>
+    <!--<script crossorigin="anonymous" async type="text/javascript" src="//api.pozvonim.com/widget/callback/v3/6ec34d3f2c2b1ff57be222c4315105fb/connect" id="check-code-pozvonim" charset="UTF-8"></script>-->
     <!--pozvonim End-->
     <?php $this->head() ?>
 </head>
